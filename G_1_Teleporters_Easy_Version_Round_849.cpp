147e94a717967ff0e692c6a59fb18c06d805be59
Abdelhman-sayed <abdelrhmansayed122003@gmail.com>
2023-06-08 09:34:09 +0300

    https://codeforces.com/contest/1791/problem/G1

---
+++ G_1_Teleporters_Easy_Version_Round_849.cpp
@@ -0,0 +1,73 @@
+// Author : abdelrhman
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <deque>
+#include <set>
+#include <cmath>
+#include <algorithm>
+
+#define ll long long
+#define forLoop(size) for (ll i = 0; i < (size); i++)
+#define forReverse(size) for (size_t i = (size - 1); i >= 0; i--)
+#define forE(v) for (auto k : v)
+#define pb(x) push_back(x)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define fj(size) for (ll j = 0; j < (size); j++)
+#define for1(size) for (int i = 1; i <= (size); i++)
+#define popCo(msk) __builtin_popcountll(msk)
+#define log(x)     __lg(x)
+#define reV(x)  return(void(cout << x << endl));
+#define fast                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define InputOutput                   \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr);
+#define EL cout << "\n";
+#define yes cout << "YES" \
+                << "\n";
+#define no cout << "NO" \
+                << "\n";
+const ll mod = 1e9 + 7, inf = 1e9, N = 1e5 + 3, M = 5e1 + 5;
+using namespace std;
+
+void boda(){
+    ll n, r;
+    cin >> n >> r;
+    vector<ll>v(n);
+    ll k = 1;
+    forLoop(n){
+        cin >> v[i];
+        v[i]+=k;
+        k++;
+    }
+    sort(all(v));
+    //cout << v << endl;
+    ll count = 0;
+    forLoop(n){
+        if(r - v[i] >= 0){
+            count++;
+            r-=v[i];
+        }
+    }
+    cout << count << endl;
+}
+int main() {
+    fast
+    InputOutput
+    // sieve();
+    // divisors();
+    ll t = 1;
+    // smallPrimeF();
+     cin >> t;
+    while (t--) {
+        boda();
+        // masking();
+        // cout << "/n__--------------------------------------------__" << endl;
+    }
+}
