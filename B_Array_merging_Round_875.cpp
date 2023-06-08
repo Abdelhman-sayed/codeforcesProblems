65978b9cdccdf51e7a19a514e8870c09d17ea1bc
Abdelhman-sayed <abdelrhmansayed122003@gmail.com>
2023-06-08 09:24:26 +0300

    https://codeforces.com/contest/1831/problem/B

---
+++ B_Array_merging_Round_875.cpp
@@ -0,0 +1,94 @@
+// Author : abdelrhman
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <deque>
+#include <set>
+#include <cmath>
+#include <algorithm>
+
+#define ll long long
+#define forLoop(size) for (ll i = 0; i < (size); i++)
+#define forReverse(size) for (size_t i = (size - 1); i >= 0; i--)
+#define forE(v) for (auto k : v)
+#define pb(x) push_back(x)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define fj(size) for (ll j = 0; j < (size); j++)
+#define for1(size) for (int i = 1; i <= (size); i++)
+#define popCo(msk) __builtin_popcountll(msk)
+#define log(x)     __lg(x)
+#define reV(x)  return(void(cout << x << endl));
+#define fast                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define InputOutput                   \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr);
+#define EL cout << "\n";
+#define yes cout << "YES" \
+                << "\n";
+#define no cout << "NO" \
+                << "\n";
+const ll mod = 1e9 + 7, inf = 1e9, N = 1e5 + 3, M = 5e1 + 5;
+using namespace std;
+
+void boda(){
+    ll n;
+    cin >> n;
+    map<ll,pair<ll, ll>>m1;
+    ll va = -1,count = 1;
+    forLoop(n){
+        ll num;
+        cin >> num;
+        if(i == 0){
+            va = num;
+            continue;
+        }else if(va == num){
+            count++;
+        }else{
+            m1[va].first = max(m1[va].first, count);
+            va = num;
+            count = 1;
+        }
+    }
+    m1[va].first = max(m1[va].first, count);
+    count = 1;
+    forLoop(n){
+        ll num;
+        cin >> num;
+        if(i == 0){
+            va = num;
+            continue;
+        }else if(va == num){
+            count++;
+        }else{
+            m1[va].second = max(m1[va].second, count);
+            va = num;
+            count = 1;
+        }
+    }
+    m1[va].second = max(m1[va].second, count);
+    ll mx = -1;
+    forE(m1){
+        mx = max(k.second.first + k.second.second, mx);
+    }
+    cout << mx << endl;
+}
+int main() {
+    fast
+    InputOutput
+    // sieve();
+    // divisors();
+    ll t = 1;
+    // smallPrimeF();
+     cin >> t;
+    while (t--) {
+        boda();
+        // masking();
+        // cout << "/n__--------------------------------------------__" << endl;
+    }
+}
