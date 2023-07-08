e2d03b88d1cdafcea7e67658b4234531cd199ac7
Abdelhman-sayed <abdelrhmansayed122003@gmail.com>
2023-07-08 23:06:43 +0300

    https://codeforces.com/contest/1846/problem/C

---
+++ C_Rudolf_and_the_Another_Competition.cpp
@@ -0,0 +1,95 @@
+// Author : abdelrhman
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <deque>
+#include <set>
+#include <cmath>
+#include <queue>
+#include <algorithm>
+
+#define ll  long long
+#define eL '\n';
+#define maxH(x) priority_queue<x>
+#define minH(x) priority_queue<x, vector<x>, greater<x>>
+#define forLoop(size) for (ll i = 0; i < (size); i++)
+#define forReverse(size) for (size_t i = (size - 1); i >= 0; i--)
+#define forE(v) for (auto k : v)
+#define pb(x) push_back(x)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define fj(size) for (ll j = 0; j < (size); j++)
+#define for1(size) for (int i = 1; i <= (size); i++)
+#define popCo(msk) __builtin_popcountll(msk)
+#define log(x)     __lg(x)
+#define reV(x)  return(void(cout << x << endl));
+#define fast                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define InputOutput                   \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr);
+#define EL cout << "\n";
+#define yes cout << "YES" \
+                << "\n";
+#define no cout << "NO" \
+                << "\n";
+const ll mod = 1e9 + 7, inf = 1e9, N = 1e5 + 3, M = 5e1 + 5;
+using namespace std;
+
+ bool com(pair<pair<ll ,ll>, ll>&a, pair<pair<ll ,ll>, ll>&b){
+     if(a.first.first > b.first.first)return true;
+     else if(a.first.first < b.first.first)return false;
+
+     if(a.first.second > b.first.second)return false;
+     else if(a.first.second < b.first.second)return true;
+
+     return a.second < b.second;
+ }
+void boda(){
+   ll p, numP, time;
+   cin >> p >> numP >> time;
+   vector<vector<ll>>v(p);
+    vector<pair<pair<ll ,ll>, ll>>final;
+    forLoop(p){
+        vector<ll>vv(numP);
+        fj(numP)cin >> vv[j];
+        sort(all(vv));
+        ll count = 0, sum = 0, points =0;
+        for (int j = 0; j < numP; ++j) {
+            if (sum + vv[j] <= time) {
+                count++;
+                sum += vv[j];
+                points += sum;
+            } else {
+                break;
+            }
+        }
+        final.push_back({{count, points}, i});
+    }
+    sort(all(final), com);
+
+    forLoop(final.size()){
+        if(final[i].second == 0){
+            cout << i+1 << endl;
+            return;
+        }
+    }
+}
+int main() {
+    fast
+    InputOutput
+    // sieve();
+    // divisors();
+    ll t = 1;
+    // smallPrimeF();
+    cin >> t;
+    while (t--){
+        boda();
+//         masking();
+        // cout << "/n__--------------------------------------------__" << endl;
+    }
+}
